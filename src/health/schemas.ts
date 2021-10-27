6579852e50c14ef6d2e32dce042132b73c0b80db
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-10-27 08:47:23 +0800

    refactor: healthy module schemas

--- src/health/schemas.ts
+++ src/health/schemas.ts
@@ -4,11 +4,17 @@ import { RouteSchema } from "../utils";
 export type Health = RouteSchema<typeof Health>;
 export const Health = new RouteSchema({
     response: {
-        "200": Type.Object({
-            message: Type.String(),
-        }),
-        "503": Type.Object({
-            message: Type.String(),
-        }),
+        "200": Type.Object(
+            {
+                message: Type.String(),
+            },
+            { additionalProperties: false, description: "Healthy repsonse" },
+        ),
+        "503": Type.Object(
+            {
+                message: Type.String(),
+            },
+            { additionalProperties: false, description: "Unhealthy response" },
+        ),
     },
 });
